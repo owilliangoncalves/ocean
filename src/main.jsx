0d6d2d9ba05231bf811fb208671251f2b8f517b8
Willian Goncalves <williangoncalves74@gmail.com>
2024-06-03 10:06:42 -0300

    criado rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
-// main.tsx or main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { NextUIProvider } from '@nextui-org/react';
 import App from './App';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './components/errorPage/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <RouterProvider router={router} />
     </NextUIProvider>
   </React.StrictMode>,
 );
